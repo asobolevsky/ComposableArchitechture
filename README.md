c7a5d9c29d0fadf3592b18e2e45815862ccb6c00
Aleksei Sobolevskii <sobolevsky.alexey@gmail.com>
2023-04-20 20:48:22 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ComposableArchitechture
+
+A description of this package.
---
+++ Sources/ComposableArchitechture/ComposableArchitechture.swift
@@ -0,0 +1,6 @@
+public struct ComposableArchitechture {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
